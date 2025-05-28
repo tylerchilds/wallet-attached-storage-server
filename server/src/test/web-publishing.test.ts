9e51e7758df49d8cbf81bcc27b5aa15b15ccf060
bengo <171782+gobengo@users.noreply.github.com>
2025-05-28 12:35:33 -0700

    revert web-publishing to before

--- server/src/test/web-publishing.test.ts
+++ server/src/test/web-publishing.test.ts
@@ -26,7 +26,7 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
 
   let spaceUuid: string | undefined
 
-  await test('web publishing with acl type PublicCanRead', async t => {
+  await test('PUT space, PUT homepage, GET it', async t => {
     const keyForAlice = await Ed25519Signer.generate()
     spaceUuid = crypto.randomUUID()
     const createPutSpaceByUuidRequest = (spaceRepresentation: unknown) => new Request(new URL(`/space/${spaceUuid}`, 'http://example.example'), {
@@ -45,145 +45,34 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
     const response = await server.fetch(request)
     assert.equal(response.status, 204, 'response status to PUT /spaces/ MUST be 204')
 
-    await t.test('PUT homepage with http sig from space controller', async t => {
-      const homepage = new Blob(['<!doctype html><h1>Home Page</h1><p>hello world<p>'], { type: 'text/html' })
-      const requestUrl = new URL(`/space/${spaceUuid}/`, 'http://example.example')
-      const requestMethod = 'PUT'
-      const responseToPutHomepage = await server.fetch(new Request(requestUrl, {
-        method: requestMethod,
-        body: homepage,
-        headers: {
-          authorization: await createHttpSignatureAuthorization({
-            signer: keyForAlice,
-            url: requestUrl,
-            method: requestMethod,
-            headers: {},
-            includeHeaders: [
-              '(created)',
-              '(expires)',
-              '(key-id)',
-              '(request-target)',
-            ],
-            created: new Date,
-            expires: new Date(Date.now() + 30 * 1000),
-          })
-        }
-      }))
-      assert.ok(
-        responseToPutHomepage.ok,
-        'response to PUT /space/:uuid/ MUST be ok')
-    })
-
-    await t.test('PUT /space/:space/links/ with acl link', async t => {
-      const linkset = {
-        linkset: [
-          {
-            anchor: `/space/${spaceUuid}/`,
-            acl: [
-              { href: `/space/${spaceUuid}/acl` },
-            ]
-          }
-        ]
-      }
-      const blobForLinkset = new Blob(
-        [JSON.stringify(linkset)],
-        { type: 'application/linkset+json' }
-      )
-      const requestUrl = new URL(`/space/${spaceUuid}/links/`, 'http://example.example')
-      const requestMethod = 'PUT'
-      const responseToPutHomepage = await server.fetch(new Request(requestUrl, {
-        method: requestMethod,
-        body: blobForLinkset,
-        headers: {
-          authorization: await createHttpSignatureAuthorization({
-            signer: keyForAlice,
-            url: requestUrl,
-            method: requestMethod,
-            headers: {},
-            includeHeaders: [
-              '(created)',
-              '(expires)',
-              '(key-id)',
-              '(request-target)',
-            ],
-            created: new Date,
-            expires: new Date(Date.now() + 30 * 1000),
-          })
-        }
-      }))
-      assert.ok(
-        responseToPutHomepage.ok,
-        `response to PUT ${requestUrl.pathname} MUST be ok`)
-    })
-
-    await t.test(`PUT PublicCanRead /space/:space/acl, then GET homepage`, async t => {
-      // set acl to PublicCanRead
-      {
-        const acl = { type: 'PublicCanRead' }
-        const blobForAcl = new Blob([JSON.stringify(acl)], { type: 'application/json' })
-        const requestUrl = new URL(`/space/${spaceUuid}/acl`, 'http://example.example')
-        const requestMethod = 'PUT'
-        const responseToPutHomepage = await server.fetch(new Request(requestUrl, {
-          method: requestMethod,
-          body: blobForAcl,
-          headers: {
-            authorization: await createHttpSignatureAuthorization({
-              signer: keyForAlice,
-              url: requestUrl,
-              method: requestMethod,
-              headers: {},
-              includeHeaders: [
-                '(created)',
-                '(expires)',
-                '(key-id)',
-                '(request-target)',
-              ],
-              created: new Date,
-              expires: new Date(Date.now() + 30 * 1000),
-            })
-          }
-        }))
-        assert.ok(
-          responseToPutHomepage.ok,
-          `response to PUT ${requestUrl.pathname} MUST be ok`)
-      }
-    })
-
-    await t.test('GET homepage sans auth (expecting acl)', async t => {
-      const requestUrl = new URL(`/space/${spaceUuid}/`, 'http://example.example')
+    await t.test('GET space', async t => {
+      const requestUrl = new URL(`/space/${spaceUuid}`, 'http://example.example')
       const requestMethod = 'GET'
       const response = await server.fetch(new Request(requestUrl, {
         method: requestMethod,
         headers: {
-          accept: 'text/html',
+          authorization: await createHttpSignatureAuthorization({
+            signer: keyForAlice,
+            url: requestUrl,
+            method: requestMethod,
+            headers: {},
+            includeHeaders: [
+              '(created)',
+              '(expires)',
+              '(key-id)',
+              '(request-target)',
+            ],
+            created: new Date,
+            expires: new Date(Date.now() + 30 * 1000),
+          })
         }
       }))
-      if (!response.ok) {
-        console.debug('response', response)
-      }
-      assert.ok(response.ok, `response to ${requestMethod} /space/:uuid/ sans auth MUST be ok`)
+      assert.ok(
+        response.ok,
+        'response to PUT /space/:uuid/ MUST be ok')
+      const objectFromResponse = await response.json()
+      assert.equal(objectFromResponse.link, spaceToCreate.link)
     })
-
-  })
-
-  await test('web publishing with acl type WAC', async t => {
-    const keyForAlice = await Ed25519Signer.generate()
-    spaceUuid = crypto.randomUUID()
-    const createPutSpaceByUuidRequest = (spaceRepresentation: unknown) => new Request(new URL(`/space/${spaceUuid}`, 'http://example.example'), {
-      method: 'PUT',
-      body: JSON.stringify(spaceRepresentation),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    const spaceToCreate = {
-      controller: keyForAlice.controller,
-      // configure pointer to links of space
-      link: `/space/${spaceUuid}/links/`,
-    }
-    const request = createPutSpaceByUuidRequest(spaceToCreate)
-    const response = await server.fetch(request)
-    assert.equal(response.status, 204, 'response status to PUT /spaces/ MUST be 204')
 
     await t.test('PUT homepage with http sig from space controller', async t => {
       const homepage = new Blob(['<!doctype html><h1>Home Page</h1><p>hello world<p>'], { type: 'text/html' })
@@ -212,6 +101,38 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
       assert.ok(
         responseToPutHomepage.ok,
         'response to PUT /space/:uuid/ MUST be ok')
+    })
+
+    await t.test('GET homepage with http sig from space controller', async t => {
+      const requestUrl = new URL(`/space/${spaceUuid}/`, 'http://example.example')
+      const requestMethod = 'GET'
+      const responseToPutHomepage = await server.fetch(new Request(requestUrl, {
+        method: requestMethod,
+        headers: {
+          authorization: await createHttpSignatureAuthorization({
+            signer: keyForAlice,
+            url: requestUrl,
+            method: requestMethod,
+            headers: {},
+            includeHeaders: [
+              '(created)',
+              '(expires)',
+              '(key-id)',
+              '(request-target)',
+            ],
+            created: new Date,
+            expires: new Date(Date.now() + 30 * 1000),
+          })
+        }
+      }))
+      if (!responseToPutHomepage.ok) {
+        console.debug('responseToPutHomepage', responseToPutHomepage)
+      }
+      assert.ok(
+        responseToPutHomepage.ok,
+        `response to ${requestMethod} /space/:uuid/ MUST be ok`)
+      const bodyOfHomepageResponse = await responseToPutHomepage.blob()
+      assert.equal(bodyOfHomepageResponse.type, 'text/html', `body type of GET response MUST be text/html`)
     })
 
     await t.test('PUT /space/:space/links/ with acl link', async t => {
@@ -256,7 +177,8 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
         `response to PUT ${requestUrl.pathname} MUST be ok`)
     })
 
-    await t.test('PUT WAC JSON acl', async t => {
+
+    await t.test('PUT acl', async t => {
       const acl = {
         authorization: [
           {
@@ -306,7 +228,7 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
     })
 
 
-    await t.test('GET homepage sans auth (expecting acl)', async t => {
+    await t.test('GET homepage sans auth (with acl set up)', async t => {
       const requestUrl = new URL(`/space/${spaceUuid}/`, 'http://example.example')
       const requestMethod = 'GET'
       const responseToPutHomepage = await server.fetch(new Request(requestUrl, {
@@ -321,7 +243,6 @@ await describe('wallet-attached-storage-server for web publishing', async t => {
       const bodyOfHomepageResponse = await responseToPutHomepage.blob()
       assert.equal(bodyOfHomepageResponse.type, 'text/html', `body type of GET response MUST be text/html`)
     })
-
   })
 })
 
