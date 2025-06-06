561fbd80700f10546cd893d909a5de48191cd0da
bengo <171782+gobengo@users.noreply.github.com>
2025-06-05 21:05:03 -0700

    wip

--- database/src/test/test-space-export-tar.test.ts
+++ database/src/test/test-space-export-tar.test.ts
@@ -6,9 +6,7 @@ import SpaceRepository from '../space-repository.ts'
 import ResourceRepository from '../resource-repository.ts'
 import type { Database, ISpace } from '../types.ts'
 import { collect } from 'streaming-iterables'
-import tar from "tar-stream"
-import { Readable, Writable } from 'stream'
-import { blob } from 'stream/consumers'
+import { exportSpaceTar, readFilesFromTar } from '../space-tar.ts'
 
 await test(`can export database as tar`, async t => {
   // setup database
@@ -45,7 +43,7 @@ await test(`can export database as tar`, async t => {
   // export space as tar
   let exportedTarStream: ReadableStream
   {
-    const exported = await exportSpaceAsTar(database, createdSpace.uuid)
+    const exported = await exportSpaceTar(new ResourceRepository(database), createdSpace.uuid)
     assert.ok(exported, `result of exportSpaceAsTar must be truthy`)
     exportedTarStream = exported
   }
@@ -58,64 +56,3 @@ await test(`can export database as tar`, async t => {
       `encoding/decoding preserves media type`)
   }
 })
-
-async function exportSpaceAsTar(database: Database, spaceUuid: string): Promise<ReadableStream> {
-  const pack = tar.pack()
-
-  // pack should contain a directory named after the space uuid
-  pack.entry({ name: spaceUuid, type: 'directory', })
-
-  const resourceRepo = new ResourceRepository(database)
-  const repsInSpace = resourceRepo.iterateSpaceNamedRepresentations({
-    space: spaceUuid,
-  })
-
-  // create entry files for each resource in space
-  for await (const rep of repsInSpace) {
-    const searchParams = rep.blob.type ? new URLSearchParams({ct:rep.blob.type.toLowerCase()}) : undefined
-    const fileName = `${rep.blob.name}${searchParams ? `?${searchParams}` : ''}`
-    const tarEntryName = `${spaceUuid}/${encodeURIComponent(fileName)}`
-    pack.entry(
-      { name: tarEntryName },
-      // @todo dont load whole blob into memory
-      Buffer.from(await rep.blob.arrayBuffer())
-    );
-  }
-  pack.finalize()
-  const t = new TransformStream();
-  (Readable.toWeb(pack) as ReadableStream).pipeThrough(t);
-  return t.readable
-}
-
-function readFilesFromTar(stream: ReadableStream) {
-  const extract = tar.extract()
-  const files = new ReadableStream<File>({
-    start(controller) {
-      extract.on('entry', (header, stream, next) => {
-        blob(stream).then(async blob => {
-          switch (header.type) {
-            case 'file':
-              const name = header.name
-              const nameParts = name.split('/').map(decodeURIComponent)
-              const entryFileName = nameParts.at(-1)
-              const entryFileNameCt = new URLSearchParams(entryFileName?.replace(/^[^?]+/,'')).get('ct') || undefined
-              controller.enqueue(new File([await blob.arrayBuffer()], header.name, { type: entryFileNameCt }))
-              break;
-            case 'directory':
-              break; // no need to make a File for directories
-            default:
-              console.warn('unsupported tar entry type', header.type, 'for entry', header.name)
-              throw new Error(`Unsupported tar entry type: ${header.type} for entry ${header.name}`);
-          }
-          next()
-        })
-      })
-      extract.on('finish', () => {
-        controller.close()
-      })
-      stream.pipeTo(Writable.toWeb(extract))
-    }
-  })
-
-  return files;
-}
